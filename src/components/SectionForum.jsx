a2cb11b714fdbd41c09ad7eb2950cc438aa9519f
Alexandre Audigié <alexandre.audigie@orange.fr>
2022-07-20 13:33:28 +0200

    forms going on

--- src/components/SectionForum.jsx
+++ src/components/SectionForum.jsx
@@ -8,21 +8,7 @@ import useFetch from "../Hooks/useFetch";
 function Posts() {
 
 
-
-
-    const [isCommentVisible, editVisibility] = useState("");
-
-
-    
-        
-
-
-
-
-
-
-    function showComment(data){
-    }
+    const [isVisible, editVisibility] = useState("");
 
 
     
@@ -36,15 +22,10 @@ function Posts() {
 
 
 
-
-
-
     const { data, loading, error } = useFetch("http://localhost:3004/Post");
     if (error) console.log(error);
 
-    
-    
-    
+
     /*
     <article id={post.postId} className="article">
         <div className="infoUser">
@@ -63,10 +44,11 @@ function Posts() {
                     <p>{post.text}</p>
                 </div>
         </article>*/
-        
 
 
-
+    function testons(post){
+        document.getElementById(post.postId).innerText = "testest "
+    };
 
 
 
@@ -90,7 +72,7 @@ function Posts() {
             <div className="sectionConnexion">
                 {
                 data?.map( post =>{ if(!post.postFollowedId){
-                    return <article id={post.postId} className="article">
+                    return <article className="article">
                     <div className="infoUser">
                         {post.profilImageUrl ? 
                             <div>
@@ -109,28 +91,30 @@ function Posts() {
                                     <img className="imagePost" src={post.postImageUrl} alt="publication du post" />
                                 </div> : ''}
                                 <p>{post.text}</p>
-                                {post.comments? <p onClick={() => showComment(data)}>Afficher les {post.comments} commentaires</p> : <p>0 commentaire</p>}
-                                {data?.map( comment=>{ if(comment.postFollowedId ===post.postId){
-                                    return (<article className="article">
-                                    <div className="infoUser">
-                                        {comment.profilImageUrl ? 
-                                            <div>
-                                                <img className="imageProfil" src={comment.profilImageUrl} alt="profil" />
-                                            </div> : <div>
-                                                        <FontAwesomeIcon className="imageProfil imageProfil--icon" icon={ faCircleUser }></FontAwesomeIcon>
-                                                    </div>}
-                                            <div>
-                                                <p className="infoUser__user">{comment.firstName + " " + comment.name}</p>
-                                                <p className="infoUser__date">{comment.date}</p>
-                                            </div>
-                                            </div>
-                                            <div className="article__corps">
-                                                <p>{comment.text}</p>
-                                            </div>
-                                    </article>)
-                                }
-                                }
+                                {post.comments? <p onClick={() => testons(post)}>Afficher les {post.comments} commentaires</p> : <p>0 commentaire</p>}
+                                <div id={post.postId}>
+                                    {data?.map( comment=>{ if(comment.postFollowedId === post.postId){
+                                        return (<article className="article">
+                                        <div className="infoUser">
+                                            {comment.profilImageUrl ? 
+                                                <div>
+                                                    <img className="imageProfil" src={comment.profilImageUrl} alt="profil" />
+                                                </div> : <div>
+                                                            <FontAwesomeIcon className="imageProfil imageProfil--icon" icon={ faCircleUser }></FontAwesomeIcon>
+                                                        </div>}
+                                                <div>
+                                                    <p className="infoUser__user">{comment.firstName + " " + comment.name}</p>
+                                                    <p className="infoUser__date">{comment.date}</p>
+                                                </div>
+                                                </div>
+                                                <div className="article__corps">
+                                                    <p>{comment.text}</p>
+                                                </div>
+                                        </article>)
+                                    }
+                                    }
                                 )}
+                                </div>
                             </div>
                     </article>
                     }
