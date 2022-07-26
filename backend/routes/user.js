b3ecd47e5840ccb8c6b55a135db6b2fa0d52305c
Alexandre Audigié <alexandre.audigie@orange.fr>
2022-07-26 13:47:33 +0200

    verify token middleware added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,12 +5,13 @@ const router = express.Router();
 const userCtrl = require('../controllers/user');
 const password = require("../middleware/password");
 const emailValidation = require("../middleware/emailValidation");
+const verifyJWT = require('../middleware/verifyJWT');
 
 
 router.post('/signup', password, emailValidation, userCtrl.signup);
 //router.post('/login', emailValidation, userCtrl.login);
 //ajouter controles sur routes ci dessous
-router.get('/', userCtrl.getAllUsers);
+router.get('/', verifyJWT, userCtrl.getAllUsers);
 //router.get('/:id', userCtrl.getOneUser);
 
 
