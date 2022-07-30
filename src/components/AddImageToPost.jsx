51e95d3bb0143408ccb3c47e188781e85d942a02
Alexandre Audigié <alexandre.audigie@orange.fr>
2022-07-30 18:39:50 +0200

    btn adding post img working nice

--- src/components/AddImageToPost.jsx
+++ src/components/AddImageToPost.jsx
@@ -5,24 +5,14 @@ import { faImage } from "@fortawesome/free-solid-svg-icons";
 
 function AddImageToPost(props) {
 
-    /*const [file, setFile] = useState("");
-    const [filename, setFileName] = useState("");*/
-
-
-
     const onChange = (e) =>{
         props.setFile(e.target.files[0]);
         props.setFilename(e.target.files[0].name);
     }
     
 
-    const uploadedImage = "";
 
-/*
-    if (props.file && props.file.type.substr(0, 5) === "image"){
-    const reader = new FileReader();
-    }*/
-
+    // Enclenche la prévisualisation de l'image lorsqu'un fichier est présent
     const [preview, showPreview] = useState("");
 
     useEffect(()=>{
